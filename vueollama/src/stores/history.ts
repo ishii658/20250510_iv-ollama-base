242dd2f9e41cc49c3f220b671ff2ac84a3e1f720
i@localhost.localdomain <i@localhost.localdomain>
2025-12-21 09:56:42 +0900

    履歴URLにもapi

--- vueollama/src/stores/history.ts
+++ vueollama/src/stores/history.ts
@@ -4,22 +4,22 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useCategoryStore = defineStore('history', () => {
-    const history = ref<{msg:string, model:string}[]>([])
+    const history = ref<{ msg: string, model: string }[]>([])
 
     /** サーバーからメモを取得
      * 
      * @param category カテゴリ
      */
-    async function get_memo(category: string, no: number){
-        const res = await axios.get("/get_memo",{params:{category: category, memoid: no}})
-        if(res.data.status == "error"){
+    async function get_memo(category: string, no: number) {
+        const res = await axios.get("/api/get_memo", { params: { category: category, memoid: no } })
+        if (res.data.status == "error") {
             alert(res.data.msg)
         }
-        else{
+        else {
             history.value = []
             const data: [string, string][] = res.data.result;
-            for(let i=0;i<data.length;i++){
-                history.value.push({model: data[i][0], msg:data[i][1]})
+            for (let i = 0; i < data.length; i++) {
+                history.value.push({ model: data[i][0], msg: data[i][1] })
             }
         }
     }
